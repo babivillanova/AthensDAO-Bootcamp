926a9fe326628b76108eaba6e4373d2bc7b481a8
Babi Villanova <babivillanova@students.iaac.net>
2022-10-11 13:10:39 -0300

    contract and frontend

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "w86pWlgnhy3yNs8EY--_hUkS82Z3CPI6";
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const GOERLI_PRIVATE_KEY = "f05a639598fc71a9ce4f3774f99ee0c8625d159233d130645ae821b29ea1dcc8";
+
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
 };
